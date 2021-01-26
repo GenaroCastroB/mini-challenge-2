6bd3d7f60382856c8eead89b29da75f61373bf36
GenaroCastroB <61991452+GenaroCastroB@users.noreply.github.com>
2021-01-25 18:31:50 -0600

    reset to initial sandbox code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
-import PokeCard from "./components/PokeCard";
-import { mockPokemonData } from "./mock/pokeData";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Welcome to your second mini challenge!</h1>
-      <PokeCard pokemonData={mockPokemonData} />
     </div>
   );
 }
